8db5254df00527854f85daad33bd346147bcdd1c
pjanik <janikpiotrek@gmail.com>
2019-01-02 11:26:59 +0100

    Add node-pre-gyp dependency (because of canvas-prebuild)
    [#162759935]

--- src/react-leaflet.d.ts
+++ src/react-leaflet.d.ts
@@ -1,6 +1,5 @@
-import * as React from "react"
-import * as L from "leaflet"
-import "react-leaflet"
+import * as L from "leaflet";
+import "react-leaflet";
 
 // react-leaflet types are outdated. Add useful extensions.
 declare module "react-leaflet" {
@@ -34,7 +33,8 @@ declare module "react-leaflet" {
 //   // https://stackoverflow.com/questions/48215950/exclude-property-from-type
 //   type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 //
-//   export function withLeaflet<T extends WrappedProps>(WrappedComponent: React.ComponentType<T>): React.ComponentType<Omit<T, 'leaflet'>>;
+//   export function withLeaflet<T extends WrappedProps>(WrappedComponent: React.ComponentType<T>):
+//   React.ComponentType<Omit<T, 'leaflet'>>;
 //
 //   export interface MapLayer {
 //     contextValue?: LeafletContext;
