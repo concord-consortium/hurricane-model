67c3d7d8cb3dfbf1e879b7a8575ea3a293d8982f
Chris Hart <chart@concord.org>
2019-03-20 15:34:36 -0400

    Fix map images on tabs

--- src/components/map-tab.tsx
+++ src/components/map-tab.tsx
@@ -4,6 +4,9 @@ import { BaseComponent, IBaseProps } from "./base";
 import { mapTypes } from "./right-panel";
 import * as css from "./map-tab.scss";
 
+import * as geoMapTab from "../assets/geo-map.png";
+import * as impactMapTab from "../assets/impact-map.png";
+
 interface IProps extends IBaseProps {
   tabType: string;
 }
@@ -17,11 +20,18 @@ export class MapTab extends BaseComponent<IProps, IState> {
     const { tabType } = this.props;
     const tabStyle = tabType === mapTypes.geo ? css.geoMaps : css.impactMaps;
     const tabText = tabType === mapTypes.geo ? "Geo Maps" : "Impact Maps";
-
+    const tabMap = {
+      backgroundImage: ""
+    };
+    if (tabType === mapTypes.geo) {
+      tabMap.backgroundImage = `url(${geoMapTab})`;
+    } else {
+      tabMap.backgroundImage = `url(${impactMapTab})`;
+    }
     return (
       <div className={`${css.mapTab} ${tabStyle}`} data-test="map-tab">
         <div className={`${css.mapTabBack} ${tabStyle}`}>
-          <div className={`${css.mapTabImage} ${tabStyle}`} />
+          <div className={`${css.mapTabImage} ${tabStyle}`} style={tabMap} />
           <div className={css.mapTabContent}>{tabText}</div>
         </div>
       </div>
