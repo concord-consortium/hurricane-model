204f6c83488778643798be5d8f3254a92c073331
Chris Hart <chart@concord.org>
2019-03-22 14:03:16 -0400

    Rework rendering

--- src/components/temperature-scale.tsx
+++ src/components/temperature-scale.tsx
@@ -1,42 +1,42 @@
 import * as React from "react";
 import Draggable from "react-draggable";
 import DragIcon from "../assets/drag.svg";
-import * as tscale from "../temperature-scale";
 import * as scaleKey from "../../sea-surface-temp-img/tempScaleKey.png";
 import * as css from "./temperature-scale.scss";
 
-interface IProps {}
+interface IProps { }
 interface IState {
   expanded: boolean;
 }
 
-const surfaceTemperature: { [key: number]: number } = {
-  0: 0,
-  1: 4,
-  2: 8,
-  3: 12,
-  4: 16,
-  5: 20,
-  6: 24,
-  7: 28,
-  8: 32
-};
 const getFahrenheit = (celcius: number) => {
   return Math.round((celcius * 9 / 5) + 32);
 };
 
-const renderTemperature = (temperature: number) => {
-  const barClass = `${css.bar} ${css[`barTemperature${temperature}`]}`;
-  const temperatureColor = tscale.temperatureScale(temperature);
-  console.log(temperatureColor);
-  return (
-    <div key={temperature} className={css.temperatureContainer}>
-      <div className={css.temperatureValue}>{ getFahrenheit(surfaceTemperature[temperature]) }</div>
-      <div className={barClass} />
-      <div className={css.dot}>.</div>
-      <div className={css.temperatureRange}>{ surfaceTemperature[temperature] }</div>
-    </div>
-   );
+const renderTemperatureLabels = (increments: number) => {
+  const celciusLabels = [];
+  const fahrenheitLabels = [];
+  for (let i = 0; i < increments; i++) {
+    celciusLabels.push(
+      <div key={"celcius" + i} className={css.temperatureContainer}>
+        <div className={css.temperatureRange}>{i * 4}</div>
+        <div className={css.dot}>.</div>
+      </div>);
+    fahrenheitLabels.push(
+      <div key={"fahrenheit" + i} className={css.temperatureContainer}>
+        <div className={css.temperatureRange}>{getFahrenheit(i * 4)}</div>
+        <div className={css.dot}>.</div>
+      </div>);
+  }
+  celciusLabels.push(<div className={css.temperatureContainer}>
+    <div className={css.temperatureValue}>&deg;C</div></div>);
+  fahrenheitLabels.push(<div className={css.temperatureContainer}>
+                <div className={css.temperatureValue}>&deg;F</div></div>);
+  return <div className={css.scaleContainer}>
+    {fahrenheitLabels}
+    <div className={css.scaleGradient} style={{ backgroundImage: `url(${scaleKey}` }} />
+    {celciusLabels}
+  </div>;
 };
 
 export class TemperatureScale extends React.PureComponent<IProps, IState> {
@@ -46,7 +46,6 @@ export class TemperatureScale extends React.PureComponent<IProps, IState> {
   }
   public render() {
     const { expanded } = this.state;
-    const temperatures = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     return (
       <div className={css.temperatureContainer}>
         <Draggable
@@ -62,17 +61,7 @@ export class TemperatureScale extends React.PureComponent<IProps, IState> {
                 <div className={css.minMaxIcon} />
               </div>
             </div>
-            {expanded &&
-              <div className={css.scaleContainer}>
-              {temperatures.map(temp => renderTemperature(temp))}
-              <div className={css.temperatureContainer}>
-                <div className={css.temperatureValue}>&deg;F</div>
-                <div className={css.spacer} />
-                <div className={css.temperatureRange}>&deg;C</div>
-                </div>
-              <div className={css.scaleGradient} style={{ backgroundImage: `url(${scaleKey}` }}/>
-              </div>
-            }
+            {expanded && renderTemperatureLabels(8)}
           </div>
         </Draggable>
       </div>
