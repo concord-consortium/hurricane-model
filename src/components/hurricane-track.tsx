c7afd295abaab784114df42f5f4a2921c094fd6c
pjanik <janikpiotrek@gmail.com>
2021-10-15 20:34:35 +0200

    Add black border to hurricane track
    [#179595544]

--- src/components/hurricane-track.tsx
+++ src/components/hurricane-track.tsx
@@ -13,10 +13,33 @@ interface IState {}
 export class HurricaneTrack extends BaseComponent<IProps, IState> {
   public render() {
     const { hurricaneTrack, hurricane } = this.stores.simulation;
-    return hurricaneTrack.map((point: ITrackPoint, idx: number) => {
-      const nextPos = idx + 1 < hurricaneTrack.length ? hurricaneTrack[idx + 1].position : hurricane.center;
-      const segmentClass = css.segment + " " + css["segmentCategory" + point.category];
-      return <Polyline key={idx} className={segmentClass} positions={[point.position, nextPos]} weight={5} />;
-    });
+
+    return (
+      // Note that "overlayPane" is below "shadowPane" in Leaflet.
+      <>
+        {
+          hurricaneTrack.map((point: ITrackPoint, idx: number) => {
+            const nextPos = idx + 1 < hurricaneTrack.length ? hurricaneTrack[idx + 1].position : hurricane.center;
+            return (
+              <Polyline key={`${idx}-border`} className={css.hurricaneTrackBorder} pane="overlayPane"
+                positions={[point.position, nextPos]} weight={7} />
+            );
+          })
+        }
+        {
+          hurricaneTrack.map((point: ITrackPoint, idx: number) => {
+            const nextPos = idx + 1 < hurricaneTrack.length ? hurricaneTrack[idx + 1].position : hurricane.center;
+            const segmentClass = css.segment + " " + css["segmentCategory" + point.category];
+            return (
+              <Polyline key={`${idx}`} className={segmentClass} pane="shadowPane"
+                positions={[point.position, nextPos]} weight={5} />
+            );
+          })
+        }
+      </>
+    );
+
+
+
   }
 }
