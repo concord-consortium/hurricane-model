371809b5830453d4e8276e7c3be7c4c72adc27d2
pjanik <janikpiotrek@gmail.com>
2024-04-08 18:47:27 +0200

    fix: delete unnecessary Cypress plugins [PT-186897291]

--- cypress.config.js
+++ cypress.config.js
@@ -7,11 +7,6 @@ module.exports = defineConfig({
   viewportHeight: 1000,
   retries: 1,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
     baseUrl: 'http://localhost:8080',
   },
 })
