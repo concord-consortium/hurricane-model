3d1cb198aa5bd8ec136ce671f43b7570490b829c
Ethan McElroy <emcelroy@concord.org>
2025-04-25 16:44:08 -0400

    chore: reconcile ICoordinates and geolocation-utils typing

--- src/models/pressure-system.ts
+++ src/models/pressure-system.ts
@@ -18,13 +18,19 @@ export interface IPressureSystemOptions {
 const minLat = 15;
 
 const minDistToOtherSystems = (center: ICoordinates, otherSystems: PressureSystem[]) => {
-  const dists = otherSystems.map(ps => distanceTo(ps.center, center));
+  const dists = otherSystems.map(ps => distanceTo(
+    {lat: ps.center.lat, lon: ps.center.lng},
+    {lat: center.lat, lon: center.lng}
+  ));
   let minDist = Infinity;
   let heading = null;
   for (let i = 0; i < dists.length; i += 1) {
     if (dists[i] < minDist) {
       minDist = dists[i];
-      heading = headingTo(center, otherSystems[i].center);
+      heading = headingTo(
+        {lat: center.lat, lon: center.lng},
+        {lat: otherSystems[i].center.lat, lon: otherSystems[i].center.lng}
+      );
     }
   }
   return { minDist, heading };
@@ -54,14 +60,16 @@ export class PressureSystem {
     wind = Object.assign({}, wind);
     const direction = this.type === "high" ? 1 : -1;
     const offset = this.type === "high" ? config.highPressureSysAngleOffset : config.lowPressureSysAngleOffset;
-    const heading = headingTo(this.center, wind) + 90 * direction - offset;
-    const distNormalized = distanceTo(this.center, wind) / this.range;
+    const ctrLat = this.center.lat;
+    const ctrLon = this.center.lng;
+    const heading = headingTo({lat: ctrLat, lon: ctrLon}, {lat: wind.lat, lon: wind.lng}) + 90 * direction - offset;
+    const distNormalized = distanceTo({lat: ctrLat, lon: ctrLon}, {lat: wind.lat, lon: wind.lng}) / this.range;
     const exp = this.type === "high" ? 0.25 : 4;
     const distExp = Math.pow(distNormalized, exp);
     const length = this.type === "high" ? distExp * this.strength : (1 - distExp) * this.strength;
-    const newPos = moveTo(wind, {distance: length, heading});
-    const u = distanceTo({lng: newPos.lng, lat: 0}, {lng: wind.lng, lat: 0}) * (newPos.lng > wind.lng ? 1 : -1);
-    const v = distanceTo({lng: 0, lat: newPos.lat}, {lng: 0, lat: wind.lat}) * (newPos.lat > wind.lat ? 1 : -1);
+    const newPos = moveTo({lat: wind.lat, lon: wind.lng}, {distance: length, heading});
+    const u = distanceTo({lat: 0, lon: newPos.lon}, {lat: 0, lon: wind.lng}) * (newPos.lon > wind.lng ? 1 : -1);
+    const v = distanceTo({lat: newPos.lat, lon: 0}, {lat: wind.lat, lon: 0}) * (newPos.lat > wind.lat ? 1 : -1);
     let newWind = { u, v };
     if (distNormalized > config.smoothPressureSystemRatio) {
       const ratio = (distNormalized - config.smoothPressureSystemRatio) / (1 - config.smoothPressureSystemRatio);
@@ -87,14 +95,18 @@ export class PressureSystem {
     center.lat = Math.max(minLat, center.lat);
     const step = config.minPressureSystemDistance / 100;
     const minDistRes = minDistToOtherSystems(center, otherPressureSystems);
-    const heading = minDistRes.heading;
+    const heading = minDistRes.heading || 0; // Provide default heading if null
     let minDist = minDistRes.minDist;
     // Why is an iterative approach used instead of a single calculation based on the minPressureSystemDistance
     // and heading? Note this single calculation could result in new center being to close to another pressure system.
     // Iterative calculations ensure that we'll always end up away from *all* the pressure systems. Initial heading
     // is used, so the interaction feels a bit more natural for user.
     while (minDist < config.minPressureSystemDistance) {
-      center = moveTo(center, { distance: step, heading: heading + 180 });
+      const newPos = moveTo(
+        {lat: center.lat, lon: center.lng},
+        { distance: step, heading: heading + 180 }
+      );
+      center = { lat: newPos.lat, lng: newPos.lon };
       minDist = minDistToOtherSystems(center, otherPressureSystems).minDist;
     }
     this.center = center;
