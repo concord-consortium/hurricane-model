78020d77744a8d0130befdd5f9c56823285f576e
Teale Fristoe <fristoe@gmail.com>
2026-05-27 09:07:49 -0700

    Fix mode and startingCategory in authoring mode.

--- src/components/authoring/authoring.tsx
+++ src/components/authoring/authoring.tsx
@@ -5,6 +5,7 @@ import { IChangeEvent } from "@rjsf/core";
 import { RJSFSchema } from "@rjsf/utils";
 import validator from "@rjsf/validator-ajv8";
 import config from "../../config";
+import { hurricaneCategoryInfo } from "../../models/constants";
 import css from "./authoring.scss";
 
 interface IProps extends IBaseProps {}
@@ -22,6 +23,14 @@ const keyTitleFormatter = ((key: string) => {
 });
 
 const customProperties: any = {
+  mode: {
+    title: "Application Mode",
+    type: "string",
+    enum: [
+      "hurricane",
+      "storm"
+    ]
+  },
   initialHurricanePosition: {
     title: "Start Position",
     type: "string",
@@ -71,6 +80,11 @@ const customProperties: any = {
       enum: config.availableOverlays
     },
     uniqueItems: true
+  },
+  startingCategory: {
+    title: "Starting Category",
+    type: "number",
+    enum: Array.from(hurricaneCategoryInfo.keys())
   }
 };
 
