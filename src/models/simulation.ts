1425b6b84ab5fbe09201d25817f1ab86f3ec0c0f
Teale Fristoe <fristoe@gmail.com>
2026-06-01 15:47:32 -0700

    Use setSeason instead of directly asigning to season in test.

--- src/models/simulation.ts
+++ src/models/simulation.ts
@@ -716,7 +716,6 @@ export class SimulationModel {
           const png = new PNG();
           png.parse(Buffer.from(buffer), (err, validPng) => {
             if (err) {
-              // eslint-disable-next-line no-console
               console.error("Failed to parse sea surface temperature PNG:", err);
               return;
             }
