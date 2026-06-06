2e83121fd6a821174df7b9d032c5c6a83f4cf9e1
Teale Fristoe <fristoe@gmail.com>
2026-06-05 18:43:20 -0700

    Add temp anomaly region data.

--- src/utils/storm-placement-region.ts
+++ src/utils/storm-placement-region.ts
@@ -1,5 +1,5 @@
 import { FeatureCollection } from "geojson";
 import { createRegion } from "./region";
-import regionData from "../assets/storm-placement-region.json";
+import regionData from "../data/regions/storm-placement-region.json";
 
 export const stormPlacementRegion = createRegion(regionData as FeatureCollection);
