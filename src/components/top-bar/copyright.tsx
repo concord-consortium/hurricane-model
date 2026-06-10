b9184cac0a5890a5897db3c72b82edeccf147d06
Teale Fristoe <fristoe@gmail.com>
2026-06-10 10:13:32 -0700

    Update license message.

--- src/components/top-bar/copyright.tsx
+++ src/components/top-bar/copyright.tsx
@@ -6,13 +6,12 @@ export class Copyright extends React.Component {
       <p style={{ fontSize: "0.8em" }}>
         <b>Copyright © {(new Date()).getFullYear()}</b> <a href="http://concord.org" target="_blank">The Concord
         Consortium</a>.
-        All rights reserved. The software is licensed under
-        the <a href="https://github.com/concord-consortium/tectonic-explorer/blob/master/LICENSE"
-               target="_blank">MIT</a> license.
-        The content is licensed under a <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">Creative
-        Commons Attribution 4.0 International License</a>.
-        Please provide attribution to the Concord Consortium and the URL <a href="http://concord.org"
-                                                                            target="_blank">http://concord.org</a>.
+        All rights reserved. This resource is licensed under
+        the <a href="https://creativecommons.org/licenses/by-nc/4.0/" target="_blank">
+        Creative Commons Attribution-NonCommercial 4.0 International License (CC BY-NC 4.0)</a>.
+        Please provide attribution to the Concord Consortium and the URL <a href="https://concord.org" target="_blank">
+        https://concord.org</a>. For full licensing details, see <a href="https://concord.org/licensing/"
+        target="_blank">concord.org/licensing</a>.
       </p>
     );
   }
