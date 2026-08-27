e59f965c26f210e5d079a9039139b9cee637f542
Teale Fristoe <fristoe@gmail.com>
2026-08-27 13:48:07 -0700

    Clean up MapTab.

--- src/components/right-panel/map-tab.tsx
+++ src/components/right-panel/map-tab.tsx
@@ -1,35 +1,35 @@
-import { inject, observer } from "mobx-react";
-import * as React from "react";
-import { BaseComponent, IBaseProps } from "../base";
+import clsx from "clsx";
+import React from "react";
 import { RightTab } from "../../models/ui";
 import css from "./map-tab.scss";
 import baseMapTabImg from "../../assets/base-map-tab.png";
 import overlayTabImg from "../../assets/overlay-tab.png";
 
-interface IProps extends IBaseProps {
+interface ITabSettings {
+  image?: string;
+  style: string;
+  text: string;
+}
+const tabSettings: Record<string, ITabSettings> = {
+  base: { style: css.geoMaps, text: "Base Maps", image: baseMapTabImg },
+  overlay: { style: css.impactMaps, text: "Map Overlays", image: overlayTabImg },
+  settings: { style: css.settings, text: "Settings" }
+};
+
+interface IProps {
   tabType: RightTab;
   active: boolean;
 }
-interface IState { }
 
-@inject("stores")
-@observer
-export class MapTab extends BaseComponent<IProps, IState> {
-
-  public render() {
-    const { tabType, active } = this.props;
-    const tabStyle = tabType === "base" ? css.geoMaps : tabType === "overlay" ? css.impactMaps : css.settings;
-    const activeStyle = active ? css.active : "";
-    const tabText = tabType === "base" ? "Base Maps" : tabType === "overlay" ? "Map Overlays" : "Settings";
-    const tabImage = tabType === "base" ? baseMapTabImg : tabType === "overlay" ? overlayTabImg : undefined;
-    return (
-      <div className={`${css.mapTab} ${tabStyle}`} data-test="map-tab">
-        <div className={`${css.mapTabBack} ${tabStyle} ${activeStyle}`}>
-          {tabImage &&
-            <div className={`${css.mapTabImage} ${tabStyle}`} style={{ backgroundImage: `url(${tabImage})` }}/>}
-          <div className={`${css.mapTabContent} ${tabImage ? "" : css.noImage}`}>{tabText}</div>
-        </div>
+export function MapTab({ active, tabType }: IProps) {
+  const { style, text, image } = tabSettings[tabType];
+  return (
+    <div className={clsx(css.mapTab, style)} data-test="map-tab">
+      <div className={clsx(css.mapTabBack, style, { [css.active]: active })}>
+        {image &&
+          <div className={clsx(css.mapTabImage, style)} style={{ backgroundImage: `url(${image})` }}/>}
+        <div className={clsx(css.mapTabContent, { [css.noImage]: !!image })}>{text}</div>
       </div>
-    );
-  }
+    </div>
+  );
 }
