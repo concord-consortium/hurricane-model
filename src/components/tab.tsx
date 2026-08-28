620eca720f5b5865d84144937855e6e4a991b7ae
Teale Fristoe <fristoe@gmail.com>
2026-08-28 16:26:29 -0700

    Make tabs buttons so they're keyboard accessible.

--- src/components/tab.tsx
+++ src/components/tab.tsx
@@ -15,15 +15,16 @@ interface IProps {
 export function Tab({ active, className, dataTest, image, onClick, side, text }: IProps) {
   const imageStyle = image ? { backgroundImage: `url(${image})` } : undefined;
   const sideClass = side === "right" ? css.right : css.left;
+  const activeClass = { [css.active]: active };
 
   return (
-    <div data-test={dataTest} className={clsx(css.panelTab, sideClass)} onClick={onClick}>
+    <button type="button" data-test={dataTest} className={clsx(css.panelTab, sideClass, activeClass)} onClick={onClick}>
       <div className={clsx(css.tab, className)} data-test={`${side}-tab`}>
-        <div className={clsx(css.tabBack, sideClass, className, { [css.active]: active })}>
+        <div className={clsx(css.tabBack, sideClass, className, activeClass)}>
           <div className={clsx(css.tabImage, className)} style={imageStyle}/>
           <div className={css.tabContent}>{text}</div>
         </div>
       </div>
-    </div>
+    </button>
   );
 }
